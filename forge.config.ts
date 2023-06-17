bb4c4f874b6d093c3395d99dd550ca4d3c3ad85e
DoWh <dinkudom@gmail.com>
2023-06-17 16:46:38 +0300

    remove /public dir, move files

--- forge.config.ts
+++ forge.config.ts
@@ -23,7 +23,7 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/public/index.html',
+            html: './src/index.html',
             js: './src/renderer.ts',
             name: 'main_window',
             preload: {
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,9 +26,9 @@
  * ```
  */
 
-import './public/index.css';
+import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
 
 // Add this to the end of the existing file
-import './index';
+import './index.tsx';
